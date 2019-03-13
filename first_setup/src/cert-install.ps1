48f5a56d493c71d3f9f929aa0ffd663a37a06fbc
NikolayM <nikolay.mashukov@adacta-group.com>
2019-03-13 22:33:25 +0300

    refactor

--- first_setup/src/cert-install.ps1
+++
@@ -1,4 +0,0 @@
-$redist = "$PSScriptRoot\..\redist"
-$pwd = ConvertTo-SecureString "idsrv3test" -AsPlainText -Force
-Import-PfxCertificate -FilePath $redist\idsrv3test.pfx -CertStoreLocation Cert:\LocalMachine\My -Password $pwd
-Start-Sleep -s 15
--- first_setup/src/mainform.ps1
+++
@@ -1,7 +0,0 @@
-$root = $PSScriptRoot
-
-[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-Add-Type -AssemblyName System.Windows.Forms
-. "$root\mainform.designer.ps1"
-
-$mainform.ShowDialog()
